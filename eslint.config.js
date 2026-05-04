f082c7a4b8a930697f2673e848b5b51dfd98cfa8
Yassir <yassir.chirawi@etu.u-pec.fr>
2026-05-04 19:12:48 +0200

    fix: ignore root utility scripts in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'functions']),
+  globalIgnores(['dist', 'functions', 'makeAdmin.js', 'list_paths.js']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
