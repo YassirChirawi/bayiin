cde461ab8648e45f1dfa815fc0baff9bcf2b562d
Yassir <yassir.chirawi@etu.u-pec.fr>
2026-05-04 19:14:16 +0200

    fix: broadly ignore non-frontend scripts in eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'functions', 'makeAdmin.js', 'list_paths.js']),
+  globalIgnores(['dist', 'functions', 'scripts', 'public/**/*.js', '*.js']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
